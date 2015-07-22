73c5b6fef491273645c4549237948f9e929df295
dcplaya <dcplaya@gmail.com>
2015-07-22 13:32:23 -0400

    Added acd_cli to do 4 simultantious connections at a time. This will eventually be an option but for now this is a test to see how it is. FUTURE ENHANCEMENT: Add option for user to change the the number of threads, maybe allow it to be set when running the script or based on # of threads of CPU (if that would make any difference)

--- Movies_Upload.sh
+++ Movies_Upload.sh
@@ -31,10 +31,10 @@ if [ "$verbose" == true ]
 then
 	echo "Verbose enabled!"
 	"$acd_cli" -v sync										# Syncs with ACD before uploading to make sure we have the most up to date info
-	"$acd_cli" -v upload "$local_location"* "$remote_location" 2> >(tee "$log_location" >&2)	# Starts uploading with the locations set at the top of this script
+	"$acd_cli" -v -x 4 upload "$local_location"* "$remote_location" 2> >(tee "$log_location" >&2)	# Starts uploading with the locations set at the top of this script
 else
 	"$acd_cli" -v sync                                              				# Syncs with ACD before uploading to make sure we have the most up to date info
-	"$acd_cli" -v upload "$local_location"* "$remote_location"       				# Starts uploading with the locations set at the top of this script
+	"$acd_cli" -v -x 4 upload "$local_location"* "$remote_location"       				# Starts uploading with the locations set at the top of this script
 fi
 
 
