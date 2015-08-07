af047c26d78b091636e58af92aa19b33894324eb
Drew Carpenter <drew@plexserver.ELCARPENTER.COM>
2015-08-07 15:18:12 -0400

    Added a file size check and an option to specify size in GB. If it exceeds that size, then the upload will not occur. This is to prevent wasted time trying to upload Proxmox backups to Amazon Cloud that is larger than their supported size. The current max size is 50GB

--- Proxmox_Backups_Upload.sh
+++ Proxmox_Backups_Upload.sh
@@ -6,13 +6,23 @@
 # Set path to acd_cli
 acd_cli=/home/drew/acd_cli/acd_cli.py
 
-#vm_number="109"
+# Set path to backups
+backups=/mnt/ProxmoxBackups/dump
+
+# Maximum size of file in GB (Gigabytes) to upload
+maxsizeGB=50
+
+############################## END config parameters #########################
+
+
+# Convert max file size from GB to bytes
+maxsize=`expr $maxsizeGB \* 1073741824`
 
 # Sync ACD first. This is a quick sync, not a full sync
 $acd_cli sync
 
 # Find all files in the dump folder and parse out which VMs they belong to
-find /mnt/Proxmox_VM_ISO/dump -type f -name '*' -print0 | while IFS= read -r -d '' file; do
+find $backups -type f -name '*' -print0 | while IFS= read -r -d '' file; do
 
         vm_name=${file#*-*-}
    	vm_number=${vm_name:0:3}
@@ -23,6 +33,18 @@ find /mnt/Proxmox_VM_ISO/dump -type f -name '*' -print0 | while IFS= read -r -d 
 	# Making directories for each VM found in the dump folder
 	$acd_cli mkdir /Proxmox/Backups/$vm_number
 
-	# Start uploading all the VM files to the respective VM folders
-	$acd_cli upload $file /Proxmox/Backups/$vm_number/
+	# Check the file size limit, dont upload if larger than specified size
+	actualsize=$(wc -c <"$file")
+	#printf '%s\n' "The size of the file is $actualsize"
+	actualsizeGB=$(echo "scale=2; $actualsize/1073741824" | bc -l) 
+
+	if [ $actualsize -lt $maxsize ]; then
+		
+		# Start uploading all the VM files to the respective VM folders
+		printf '%s\n' "[INFO] - File size $actualsizeGB GB < $maxsizeGB GB"
+		$acd_cli upload $file /Proxmox/Backups/$vm_number/
+	else
+		printf '%s\n' "[WARNING] - File size larger than specified"
+	fi
+			
 done
